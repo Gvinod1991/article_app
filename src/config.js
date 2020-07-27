28dd6d4a3ff6cb517f69feace6562c1216489316
Godti Vinod <venkat.godti3@gmail.com>
2020-07-27 22:51:02 +0530

    api enpoint updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config={
-    apiEndpoint:'http://localhost:2004/'
+    //apiEndpoint:'http://localhost:2004/'
+    apiEndpoint:'http://article-api-app.herokuapp.com/'
 }
 
 export default config
